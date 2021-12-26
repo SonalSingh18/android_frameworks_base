2b90455c3d9fd3fb9047979780a0fe80e0b41ff1
Pranav Vashi <neobuddy89@gmail.com>
2021-12-27 00:20:31 +0900

    Allow user to select low power refresh rate [1/2]
    * This patch is based on:
      https://github.com/crdroidandroid/android_frameworks_base/commit/9d4b0238e0c2caa810bdbf1c3d5e1c65838ef2e4
    * With newer devices supporting refresh rates lower than 60 Hz,
      let user customize this further.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/display/mode/DisplayModeDirector.java
+++ services/core/java/com/android/server/display/mode/DisplayModeDirector.java
@@ -1114,11 +1114,14 @@ public class DisplayModeDirector {
         }
 
         private void updateLowPowerModeSettingLocked() {
-            mIsLowPower = Settings.Global.getInt(mContext.getContentResolver(),
+            final ContentResolver cr = mContext.getContentResolver();
+            mIsLowPower = Settings.Global.getInt(cr,
                     Settings.Global.LOW_POWER_MODE, 0 /*default*/) != 0;
             final Vote vote;
             if (mIsLowPower) {
-                vote = Vote.forRenderFrameRates(0f, 60f);
+                float lowPowerRefreshRate = Settings.System.getFloatForUser(cr,
+                    Settings.System.LOW_POWER_REFRESH_RATE, 60f /*default*/, cr.getUserId());
+                vote = Vote.forRenderFrameRates(0f, lowPowerRefreshRate);
             } else {
                 vote = null;
             }
