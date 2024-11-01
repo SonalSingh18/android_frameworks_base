11cf0ae70789a296e9bca4412853849b1ff73c6b
Pranav Vashi <neobuddy89@gmail.com>
2024-11-01 10:28:57 +0530

    SystemUI: Allow tuning extra padding for statusbar layout [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7290,6 +7290,21 @@ public final class Settings {
         public static final String ONGOING_ACTION_CHIP = "ongoing_action_chip";
 
         /**
+         * @hide
+         */
+        public static final String STATUSBAR_EXTRA_PADDING_START = "statusbar_extra_padding_start";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_EXTRA_PADDING_TOP = "statusbar_extra_padding_top";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_EXTRA_PADDING_END = "statusbar_extra_padding_end";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
